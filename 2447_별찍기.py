1ae7304c13004b8b1f1b31895b2eb643fd151de7
mack1210 <37128035+mack1210@users.noreply.github.com>
2022-04-01 16:21:00 +0900

    Made it, but have to try it without using NUMPY

--- 2447_별찍기.py
+++ 2447_별찍기.py
@@ -1,22 +1,70 @@
+# numpy를 쓰면 백준에서는 런타임에러가 발생한다.
+import numpy as np
+from time import time
 
-
-def pattern(N):
-    for i in range(N):
-        if i%3 == 1:
-            print("* *"*int(N/3))
-        else:
-            print("***"*int(N/3))
-            
-   
-
-def make_stars(N):
-    import math
-    k = int(math.log(N, 3))
-    if N == 3:
-        pattern(N)
+def make_starts(N):
+    star = np.zeros((N,N))
+    
+    pattern = [[1,1,1], [1,0,1], [1,1,1]]
+    N_prime = int(N/3)
+    
+    if N_prime == 1:
+        star = pattern
     else:
-        pattern(N)
+        for i in range(0, N, N_prime):
+            for j in range(0, N, N_prime):
+                if i == N_prime and j == N_prime:
+                    continue
+                else:
+                    star[i:i+N_prime, j:j+N_prime] = make_starts(N_prime)
+    return star
+
+N = int(input())
+start_time = time()
+
+stars= make_starts(N)
+for i in range(N):
+    for j in range(N):
+        if stars[i,j] == 1:
+            print("*", end = " ")
+        else:
+            print(" ", end= " ")
+    print()
+print(time()-start_time)
 
 
-N = 27 # int(input()
-make_stars(N) 
+
+# numpy를 쓰면 백준에서는 런타임에러가 발생한다.
+# import numpy as np
+from time import time
+
+def make_starts(N):
+    star = [[0]*N]*N
+    
+    pattern = [[1,1,1], [1,0,1], [1,1,1]]
+    N_prime = int(N/3)
+    
+    if N_prime == 1:
+        star = pattern
+    else:
+        for i in range(0, N, N_prime):
+            for j in range(0, N, N_prime):
+                if i == N_prime and j == N_prime:
+                    continue
+                else:
+                    star = make_starts(N_prime)
+                    star[int(i%3)][int(j%3)] = star[i:i+N_prime][j:j+N_prime]
+    return star
+
+N = 9# int(input())
+start_time = time()
+
+stars= make_starts(N)
+for i in range(N):
+    for j in range(N):
+        if stars[i][j] == 1:
+            print("*", end = " ")
+        else:
+            print(" ", end= " ")
+    print()
+print(time()-start_time)
