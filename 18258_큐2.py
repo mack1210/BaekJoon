118d2ab459ec9093a8d1605d60cd06b40c4bf3cc
mack1210 <37128035+mack1210@users.noreply.github.com>
2022-04-10 21:16:15 +0900

    Update 18258_큐2.py
    Succeed!
    Using dictionary and index

--- 18258_큐2.py
+++ 18258_큐2.py
@@ -62,53 +62,112 @@
 
 
 
-## deque를 사용하여 시간초과 에러 해결
-## subclass 개념을 도입하여 시도
+### deque를 사용하여 시간초과 에러 해결
+### subclass 개념을 도입하여 시도
 
+# import sys
+# from collections import deque
+
+# class Que(deque):
+#     def __init__(self):
+#         self._queue = super()
+#         self.lst = []
+        
+#         self.i_start = 0
+    
+#     def push(self, X):
+#         self._queue.append(X)
+#         self.lst.append(X)
+    
+#     def popleft(self):
+#         if self.size():
+#             print(self._queue.popleft())
+#             self.i_start += 1
+#         else:
+#             print(-1)
+        
+#     def lenth(self):
+#         return len(self.lst)
+
+#     def size(self):
+#         return len(self.lst[self.i_start:])
+    
+#     def empty(self):
+#         if self.i_start == self.lenth():
+#             return 1
+#         else:
+#             return 0
+    
+#     def front(self):
+#         if self.empty():
+#             return -1
+#         else:
+#             return self.lst[self.i_start]
+    
+#     def back(self):
+#         if self.empty():
+#             return -1
+#         else:
+#             return self.lst[-1]
+    
+
+# N = int(input())
+# q = Que()
+
+# for i in range(N):
+#     inputs = sys.stdin.readline().split()
+#     order = inputs[0]
+    
+#     if len(inputs) == 2 and order == 'push':
+#         q.push(inputs[1])
+#     elif order == 'pop':
+#         q.popleft()
+#     else:
+#         x = getattr(q, order)
+#         print(x())
+
+### 또 실패하여 dictionary를 도입해 풀어보았다.
 import sys
-from collections import deque
 
-class Que(deque):
+class Que():
     def __init__(self):
-        self._queue = super()
-        self.lst = []
+        self.que = {}
         
         self.i_start = 0
+        self.i_total = 0
     
     def push(self, X):
-        self._queue.append(X)
-        self.lst.append(X)
+        self.que[self.i_total] = X
+        self.i_total += 1
     
-    def popleft(self):
-        if self.size():
-            print(self._queue.popleft())
-            self.i_start += 1
-        else:
-            print(-1)
-        
-    def lenth(self):
-        return len(self.lst)
-
     def size(self):
-        return len(self.lst[self.i_start:])
+        return self.i_total - self.i_start
     
-    def empty(self):
-        if self.i_start == self.lenth():
-            return 1
+    def pop(self):
+        if self.i_total - self.i_start:
+            pop_val = self.que[self.i_start]
+            self.i_start += 1
+            return pop_val
         else:
+            return -1
+
+    def empty(self):
+        if self.i_total - self.i_start:
             return 0
+        else:
+            return 1
     
     def front(self):
-        if self.empty():
-            return -1
+        if self.i_total - self.i_start:
+            return self.que[self.i_start]
         else:
-            return self.lst[self.i_start]
+            return -1
     
     def back(self):
-        if self.empty():
-            return -1
+        if self.i_total - self.i_start:
+            return self.que[self.i_total-1]
         else:
-            return self.lst[-1]
+            return -1
     
 
 N = int(input())
@@ -120,8 +179,6 @@ for i in range(N):
     
     if len(inputs) == 2 and order == 'push':
         q.push(inputs[1])
-    elif order == 'pop':
-        q.popleft()
     else:
         x = getattr(q, order)
         print(x())
